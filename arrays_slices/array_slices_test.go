f23737db2ccc89491bb4ba54640df07ffa31b661
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-07 17:40:06 +0100

    adds TDD for sum function for varying number of slices

--- arrays_slices/array_slices_test.go
+++ arrays_slices/array_slices_test.go
@@ -16,4 +16,13 @@ func TestSum(t *testing.T) {
 
 		assertCorrectMessage(t, got, expected, xs)
 	})
+
+	t.Run("collection of varying number of slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		expected := []int{3, 9}
+		if got != expected {
+			t.Errorf("got %v expected %v", got, expected)
+		}
+
+	})
 }
