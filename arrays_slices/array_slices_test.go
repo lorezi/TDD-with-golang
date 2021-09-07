940db8686d9f58a42a55adb8e3e9812c0535584f
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-07 16:53:50 +0100

    adds TDD for sum of slices

--- arrays_slices/array_slices_test.go
+++ arrays_slices/array_slices_test.go
@@ -2,11 +2,22 @@ package arraysslices
 
 import "testing"
 
-func TestArrayslice(t *testing.T) {
+func TestArray(t *testing.T) {
 	xs := [5]int{1, 2, 3, 4, 5}
 	got := Sum(xs)
 	expected := 15
 
+	if got != expected {
+		t.Errorf("expected '%d' got '%d' given, %v", expected, got, xs)
+	}
+
+}
+
+func TestSlice(t *testing.T) {
+
+	xs := []int{1, 2, 3}
+	got := SumN(xs)
+	expected := 6
 	if got != expected {
 		t.Errorf("expected '%d' got '%d' given, %v", expected, got, xs)
 	}
