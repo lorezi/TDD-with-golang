db1533283455421f29bce51dea6041523659c982
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-07 16:22:21 +0100

    modifies the docs

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,6 +1,6 @@
 package iteration
 
-// Iteration takes a character and number of iteration and returns x5 of the character
+// Iteration takes a character and number of iteration and returns (n)times of the character
 func Iteration(character string, n int) string {
 	repeated := ""
 	for i := 0; i < n; i++ {
