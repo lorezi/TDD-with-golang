0b1f8798d4ba92eda41c826b0c811a0aadec3775
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-07 11:47:47 +0100

    refactors to use constant

--- hello/hello.go
+++ hello/hello.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	return englishHelloPrefix + name
 }
 
 func main() {
