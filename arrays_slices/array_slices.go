d4e44a1e98cdc9f7d25a7bc30fd17128c7677a1e
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-07 16:46:48 +0100

    refactors the sum of array

--- arrays_slices/array_slices.go
+++ arrays_slices/array_slices.go
@@ -3,8 +3,8 @@ package arraysslices
 // Sum will take an array of numbers and return the total
 func Sum(xs [5]int) int {
 	total := 0
-	for i := 0; i < 5; i++ {
-		total += xs[i]
+	for _, v := range xs {
+		total += v
 	}
 	return total
 }
