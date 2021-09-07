9397e6186f4acd37bea2a6c805515a55d14cb74a
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-07 11:04:48 +0100

    adds hello world function and test suite

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lorezi/learn_go_with_test
+
+go 1.15
---
+++ hello/hello_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	/*
+		Arrange
+		Assert
+		Act
+	*/
+
+	// arrange
+	got := Hello()
+	want := "Hello, world"
+
+	// assert
+	if got != want {
+
+		// act
+		t.Errorf("got %q want %q", got, want)
+	}
+}
