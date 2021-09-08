29be37d7c8e82bb57f02ccba4baba4f2f3012570
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-08 20:32:46 +0100

    adds minimal code to pass the test

--- pointers_errors/pointers_errors.go
+++ pointers_errors/pointers_errors.go
@@ -4,12 +4,10 @@ type Wallet struct {
 	balance int
 }
 
-func (w Wallet) Deposit(amount int) {
-
+func (w *Wallet) Deposit(amount int) {
+	w.balance += amount
 }
 
 func (w Wallet) Balance() int {
-	w.balance = 10
-
 	return w.balance
 }
