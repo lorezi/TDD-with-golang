14de9579d99b8155692d13e9e9ea9e8abb674a77
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-08 11:46:46 +0100

    refactors the code and test suite

--- struct_methods_interfaces/struct_methods_interfaces.go
+++ struct_methods_interfaces/struct_methods_interfaces.go
@@ -1,10 +1,15 @@
 package structmethodsinterfaces
 
-func Perimeter(width float64, height float64) float64 {
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Perimeter(width float64, height float64) float64 {
 	return 2 * (width + height)
 }
 
-func Area(width, height float64) float64 {
+func (r Rectangle) Area(width, height float64) float64 {
 
-	return 72.0
+	return width * height
 }
--- struct_methods_interfaces/struct_methods_interfaces_test.go
+++ struct_methods_interfaces/struct_methods_interfaces_test.go
@@ -3,7 +3,11 @@ package structmethodsinterfaces
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.00, 10.00)
+	r := Rectangle{
+		Width:  10.00,
+		Height: 10.00,
+	}
+	got := r.Perimeter(10.00, 10.00)
 	want := 40.00
 
 	if got != want {
@@ -12,7 +16,11 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.00, 6.0)
+	r := Rectangle{
+		Width:  12.00,
+		Height: 6.0,
+	}
+	got := r.Area(12.00, 6.0)
 	want := 72.0
 
 	if got != want {
