24e13f4b449f9ec20e7428f115d2395cd6c2f463
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-08 16:19:58 +0100

    creates the code and test file

---
+++ pointers_errors/pointers_errors_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	testCases := []struct {
+		desc string
+	}{
+		{
+			desc: "",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+
+		})
+	}
+}
