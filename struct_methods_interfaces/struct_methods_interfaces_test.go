a6b72107c19b79a63fbb129eec910ac002b5af02
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-08 15:23:23 +0100

    refactors the test suite to use table driven test TDT

--- struct_methods_interfaces/struct_methods_interfaces_test.go
+++ struct_methods_interfaces/struct_methods_interfaces_test.go
@@ -17,26 +17,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	/* Table Driven Test TDT */
+
+	// Anonymous struct
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		r := Rectangle{12, 6}
-		want := 72.0
-		checkArea(t, r, want)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		c := Circle{10}
-		want := 314.1592653589793
-
-		checkArea(t, c, want)
-
-	})
+	for _, v := range areaTests {
+		got := v.shape.Area()
+		if got != v.want {
+			t.Errorf("got %g want %g", got, v.want)
+		}
+	}
 
 }
