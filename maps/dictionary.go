bbf3c56b8deeb3ac06cdf50b0a641c795164e61c
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-09 12:59:01 +0100

    adds minimal code to pass the test

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,6 @@
 package main
 
-func Search(dictionary map[string]string, definition string) string {
-	return ""
+func Search(dictionary map[string]string, word string) string {
+
+	return dictionary[word]
 }
