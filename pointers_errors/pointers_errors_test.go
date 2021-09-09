71b2801f2a8bd3fc8e6a0c1f1a80191e8228f1c2
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-09 10:35:27 +0100

    adds TDD for the withdraw operation

--- pointers_errors/pointers_errors_test.go
+++ pointers_errors/pointers_errors_test.go
@@ -3,13 +3,27 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := Bitcoin(11)
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Withdraw(Bitcoin(5))
+
+		got := wallet.Balance()
+		want := Bitcoin(5)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
