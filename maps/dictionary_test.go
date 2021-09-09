684dce6237a4632c0671e11af5a16886807fe666
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-09 12:34:05 +0100

    adds TDD for dictionary search operation

---
+++ maps/dictionary_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := dictionary["test"]
+	want := "this is just a test"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+}
