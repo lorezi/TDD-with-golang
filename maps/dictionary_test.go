0fd1cc57b6df1ba6ceece97fb797c046d12905e9
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-09 13:02:52 +0100

    refactors the test suite

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -10,8 +10,13 @@ func TestSearch(t *testing.T) {
 	got := Search(dictionary, "test")
 	want := "this is just a test"
 
+	assertStrings(t, got, want)
+
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
-
 }
