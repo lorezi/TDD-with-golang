6b87cec6c9d4395b6fdcd6052cb96346ec156861
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-09 11:08:03 +0100

    adds minimal code to pass the test

--- pointers_errors/pointers_errors.go
+++ pointers_errors/pointers_errors.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -21,6 +24,9 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 }
 
 func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
 	w.balance -= amount
 	return nil
 }
