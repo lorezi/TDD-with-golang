521d8b9bcc9bbf41ebf7ffc37b5f318425ce761f
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-09 11:19:26 +0100

    refactors the test suite

--- pointers_errors/pointers_errors_test.go
+++ pointers_errors/pointers_errors_test.go
@@ -10,6 +10,16 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s want %s", got, want)
 		}
 	}
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("wanted an error but didn't get one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -29,10 +39,9 @@ func TestWallet(t *testing.T) {
 	t.Run("Withdraw insufficient fund", func(t *testing.T) {
 		startingBalance := Bitcoin(20)
 		wallet := Wallet{balance: startingBalance}
-		err := wallet.Withdraw(30)
+		got := wallet.Withdraw(30)
 		assertBalance(t, wallet, startingBalance)
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
-		}
+		assertError(t, got, "cannot withdraw, insufficient funds")
+
 	})
 }
