574ff08552411a2a3e8bffb04c10949a97786e3d
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-10 16:57:33 +0100

    adds mocks to the test suite

--- mocks/mock.go
+++ mocks/mock.go
@@ -10,6 +10,10 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
+type Sleeper interface {
+	Sleep()
+}
+
 func Countdown(w io.Writer) {
 	for i := countdownStart; i > 0; i-- {
 		time.Sleep(1 * time.Second)
--- mocks/mock_test.go
+++ mocks/mock_test.go
@@ -5,10 +5,19 @@ import (
 	"testing"
 )
 
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := `3
@@ -18,5 +27,9 @@ Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 4 {
+		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
 	}
 }
