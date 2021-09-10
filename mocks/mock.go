06c6f656a2faf0fe20e64f3353b82f17e3a6e92d
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-10 16:31:37 +0100

    this task was broken down into smaller tasks

--- mocks/mock.go
+++ mocks/mock.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWord = "Go!"
@@ -11,8 +12,10 @@ const countdownStart = 3
 
 func Countdown(w io.Writer) {
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintf(w, "%d\n", i)
 	}
+	time.Sleep(1 * time.Second)
 	fmt.Fprint(w, finalWord)
 }
 
