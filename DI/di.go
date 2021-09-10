f41e937cb7c6985bf5bca9bd9699ad9a587c7bf7
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-10 15:48:56 +0100

    refactors the code

--- DI/di.go
+++ DI/di.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreetHandler)))
 }
