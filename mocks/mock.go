559fa2abf48477242ea0ee35692cd2475b35d056
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-10 16:26:33 +0100

    refactors the code

--- mocks/mock.go
+++ mocks/mock.go
@@ -6,11 +6,14 @@ import (
 	"os"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(w io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintf(w, "%d\n", i)
 	}
-	fmt.Fprint(w, "Go!")
+	fmt.Fprint(w, finalWord)
 }
 
 func main() {
