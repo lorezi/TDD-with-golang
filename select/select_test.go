9b9c5e06b1ea0eff17b4b09cf13fc95508605b53
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-13 17:39:15 +0100

    adds enough code to make the test pass

--- select/select_test.go
+++ select/select_test.go
@@ -17,16 +17,34 @@ func server(delay time.Duration) *httptest.Server {
 }
 
 func TestRacer(t *testing.T) {
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowSever := server(20)
+		fastServer := server(0)
 
-	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		defer slowSever.Close()
+		defer fastServer.Close()
 
-		serverA := server(11)
-		serverB := server(12)
+		want := fastServer.URL
 
-		defer serverA.Close()
-		defer serverB.Close()
+		got, err := Racer(slowSever.URL, fastServer.URL)
 
-		_, err := Racer(serverA.URL, serverB.URL, 9)
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+	})
+
+	t.Run("returns an error if a server doesn't respond within specified time", func(t *testing.T) {
+
+		server := server(25)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
 
 		if err == nil {
 			t.Errorf("expected an error but didn't get one")
