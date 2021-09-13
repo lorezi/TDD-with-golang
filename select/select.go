4937f9ceb54dc35fe4809a36690b9456d0872766
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-13 15:48:31 +0100

    adds minimal code to pass the test

--- select/select.go
+++ select/select.go
@@ -1,5 +1,23 @@
 package main
 
-func Racer(slowURL string, fastURL string) string {
-	return ""
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string) {
+
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
 }
