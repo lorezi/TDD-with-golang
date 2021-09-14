f8981a7c2dcd75d653b71df35724f71e6d4c5532
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-14 09:44:51 +0100

    adds TDD for the fucntion

--- reflection/reflect_test.go
+++ reflection/reflect_test.go
@@ -1,1 +1,20 @@
 package main
+
+import "testing"
+
+func TestWalk(t *testing.T) {
+	expected := "Lorezi"
+	var got []string
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	}
+}
