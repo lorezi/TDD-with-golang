5dbc783b8e9b2ad05850ca1fbdbeec6bee99cc7b
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-14 10:38:09 +0100

    refactors the test suite to use table driven test TDT

--- reflection/reflect_test.go
+++ reflection/reflect_test.go
@@ -1,24 +1,35 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	expected := "Lorezi"
-	var got []string
-
-	x := struct {
-		Name string
-	}{expected}
-
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	testCases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Lawrence"},
+			[]string{"Lawrence"},
+		},
 	}
+	for _, tC := range testCases {
+		t.Run(tC.Name, func(t *testing.T) {
+			var got []string
+			walk(tC.Input, func(input string) {
+				got = append(got, input)
+			})
 
-	if got[0] != expected {
-		t.Errorf("got %q, want %q", got[0], expected)
+			if !reflect.DeepEqual(got, tC.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, tC.ExpectedCalls)
+			}
+
+		})
 	}
 }
