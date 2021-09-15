39db344019b79622e145c452429e0560db8fe5f9
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-15 09:58:49 +0100

    adds minimal code to pass the test

--- reflection/reflect.go
+++ reflection/reflect.go
@@ -4,6 +4,11 @@ import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
