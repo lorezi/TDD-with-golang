06e77d0a3723c00d2056f4984861ba34bdfdc9f4
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-15 15:21:10 +0100

    you enjoy Go when you write syntatic sugar code

--- reflection/reflect.go
+++ reflection/reflect.go
@@ -29,10 +29,7 @@ func walk(x interface{}, fn func(input string)) {
 	case reflect.Struct:
 		numberOfValues = val.NumField()
 		getField = val.Field
-	case reflect.Slice:
-		numberOfValues = val.Len()
-		getField = val.Index
-	case reflect.Array:
+	case reflect.Slice, reflect.Array:
 		numberOfValues = val.Len()
 		getField = val.Index
 	}
