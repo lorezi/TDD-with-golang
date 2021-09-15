10c49e6ad2a4445395afa1e572c21c22019f4a69
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-15 15:51:54 +0100

    refactors the test suite

--- reflection/reflect.go
+++ reflection/reflect.go
@@ -19,27 +19,25 @@ func walk(x interface{}, fn func(input string)) {
 
 	val := getValue(x)
 
-	numberOfValues := 0
-
-	var getField func(int) reflect.Value
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
 
 	switch val.Kind() {
 	case reflect.String:
 		fn(val.String())
 	case reflect.Struct:
-		numberOfValues = val.NumField()
-		getField = val.Field
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
 	case reflect.Slice, reflect.Array:
-		numberOfValues = val.Len()
-		getField = val.Index
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
 	case reflect.Map:
 		for _, key := range val.MapKeys() {
-			walk(val.MapIndex(key).Interface(), fn)
+			walkValue(val.MapIndex(key))
 		}
-	}
-
-	for i := 0; i < numberOfValues; i++ {
-		walk(getField(i).Interface(), fn)
 	}
 
 }
