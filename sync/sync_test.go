3eb14478825115fe988152fd2a5bbc5d02f437af
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-20 20:32:52 +0100

    adds TDD for incrementing the counter

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,1 +1,17 @@
+// The idea is to access the counter unsafely with multiple goroutines
 package main
+
+import "testing"
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times leaves it at 3,", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		if counter.Value() != 3 {
+			t.Errorf("got %d, want %d", counter.Value(), 3)
+		}
+	})
+}
