44e88c8673c961fd40ddd1f2da3f064f6f24b271
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-20 21:56:24 +0100

    adds mutex to create exclusive shared memory access

--- sync/sync.go
+++ sync/sync.go
@@ -1,10 +1,16 @@
 package main
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
+// mutex creates an exclusive shared memory
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
