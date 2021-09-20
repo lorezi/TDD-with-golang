9ea88fba7a0c4f477d596cb892e2af6a47b5e473
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-20 21:44:18 +0100

    adds TDD for concurrency

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,7 +1,10 @@
-// The idea is to access the counter unsafely with multiple goroutines
+// The idea is to access the counter safely with multiple goroutines
 package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3,", func(t *testing.T) {
@@ -11,6 +14,23 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 
 		assertCounter(t, counter, 3)
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
 	})
 }
 
