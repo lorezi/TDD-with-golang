b92f57e5a1f848d43cb5fef76d80b3987f22caab
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-21 23:29:43 +0100

    refactors the test suite

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Test Driven Development With Go
 
-Fintech ❤️ Go - Chris James
+Why the stress 😓 of TDD 🤷🏽‍♂️🤷🏽‍♂️🤷🏽‍♂️
+
+1. Confidence you can reshape code without worrying about changing behaviour
+2. Documentation for humans as to how the system should behave
+3. Much faster & more reliable feedback than manual testing
--- sync/sync_test.go
+++ sync/sync_test.go
@@ -6,9 +6,13 @@ import (
 	"testing"
 )
 
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3,", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
@@ -18,7 +22,7 @@ func TestCounter(t *testing.T) {
 
 	t.Run("it runs safely concurrently", func(t *testing.T) {
 		wantedCount := 1000
-		counter := Counter{}
+		counter := NewCounter()
 
 		var wg sync.WaitGroup
 		wg.Add(wantedCount)
@@ -34,7 +38,7 @@ func TestCounter(t *testing.T) {
 	})
 }
 
-func assertCounter(t testing.TB, got Counter, want int) {
+func assertCounter(t testing.TB, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), want)
