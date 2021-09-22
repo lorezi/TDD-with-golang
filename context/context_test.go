e6b601d6fe97afa1d82ac6c498bfecb8d7ecee43
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-22 15:57:35 +0100

    refactors the test suite

--- context/context_test.go
+++ context/context_test.go
@@ -68,7 +68,7 @@ func TestServer(t *testing.T) {
 		svr.ServeHTTP(response, request)
 
 		if !store.cancelled {
-			t.Errorf("store was not told to cancel")
+			t.Errorf("store was not told to cancel 🥵")
 		}
 		store.assertWasCancelled()
 	})
