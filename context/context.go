4d48f7e7e75810b38952f3cbe92279a4f82f9d40
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-09-22 15:07:13 +0100

    adds minimal code pass the test

--- context/context.go
+++ context/context.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		store.Cancel()
+		fmt.Fprintf(rw, store.Fetch())
+	}
+}
